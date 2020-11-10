2a3fcbf06ca33d852187fae8d2ed3149d3a4a766
Paulo <paulohincar@gmail.com>
2020-11-10 12:58:09 -0300

    :up: cover image -> app.component

--- desafio-github/src/app/services/usuario.service.ts
+++ desafio-github/src/app/services/usuario.service.ts
@@ -1,10 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-// import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
-import { BehaviorSubject } from 'rxjs';
-import { Observable } from 'rxjs';
-// import { map } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 // interface user {
 //   login: string;
