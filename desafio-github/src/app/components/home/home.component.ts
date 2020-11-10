77b0a469dbe98cb964652093afb5dee7a70ffdb9
Paulo <paulohincar@gmail.com>
2020-11-10 01:06:20 -0300

    :up: mudanças no visual e na responsividade

--- desafio-github/src/app/components/home/home.component.ts
+++ desafio-github/src/app/components/home/home.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { UsuarioService } from '../../services/usuario.service'
 import { Router, ActivatedRoute } from '@angular/router'
+import { MatDialog } from '@angular/material/dialog'
+import { ErrorDialogComponent } from '../../dialogs/error-dialog/error-dialog.component'
+
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +12,15 @@ import { Router, ActivatedRoute } from '@angular/router'
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  values = '';
+  values = '';  
+  faPesquisa = faSearch;
 
-  constructor(private usuarioService:UsuarioService, private router:Router, private route:ActivatedRoute) {
+  constructor(private usuarioService:UsuarioService, private router:Router, private route:ActivatedRoute, public dialog: MatDialog) {
 
+  }
+
+  openDialog() {
+    this.dialog.open(ErrorDialogComponent);
   }
 
   onKey(event: any){
@@ -19,12 +28,23 @@ export class HomeComponent implements OnInit {
   }
 
   enterPress(url){
+
+    this.usuarioService.getProfileInfo(this.values).subscribe(
+      () => {
+        this.router.navigate([url,this.values]);
+      },
+      () => {
+        this.openDialog();
+      }
+    );
+
+
     // this.usuarioService.getProfileInfo(this.values).subscribe(usuario => {
     //   this.usuario = usuario;         
     // });
     // this.usuarioService.currentData.subscribe(data => )
-    this.usuarioService.changeData(this.values);
-    this.router.navigate([url,this.values]);
+    // this.usuarioService.changeData(this.values);
+
   }
 
   ngOnInit(): void {
