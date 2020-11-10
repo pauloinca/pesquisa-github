d43c5f7d23194c13482043f98bdb843fb3bf71b7
Paulo <paulohincar@gmail.com>
2020-11-10 16:58:27 -0300

    :up:

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { BehaviorSubject } from 'rxjs';
 
 // interface user {
 //   login: string;
